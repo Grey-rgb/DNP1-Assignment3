e3d3e04152d666a647c7c9f0f16905d9d52ed03f
mads8 <madsfroeslev@gmail.com>
2022-09-30 22:01:39 +0200

    User class baseline, non-functional
    The user class has been created along with it's setters, getters and
    constructor. This is so that I can implement further user
    functionality in relation to posts and such.
    
    Authored by: Mads Bobjerg Jakobsen (316452@via.dk)

---
+++ Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models;
+
+public class User
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public List<Post> Posts = new List<Post>();
+
+    public User(string userName, string password)
+    {
+        this.UserName = userName;
+        this.Password = password;
+    }
+}
